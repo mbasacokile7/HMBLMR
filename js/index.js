e27b7cd217ece08a58dbb37e38c8016c37b15a4c
Mbasa Cokile <60657016+mbasacokile7@users.noreply.github.com>
2022-05-13 14:14:38 +0200

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  Particles.init({
+    selector: '.background',
+    connectParticles: true,
+    color: '#A020F0',
+    maxParticles: 300,
+    sizeVariations: 4,
+  });
+};
+ 
